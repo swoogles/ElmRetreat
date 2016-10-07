ed872fa852ecd41811847675ffafc0dff01a162e
Bruce Eckel <mindviewinc@gmail.com>
2016-10-07 15:32:44 -0600

    two squares with words

--- Squares/Square.elm
+++ Squares/Square.elm
@@ -3,11 +3,21 @@ module Square exposing (..)
 import Color exposing (..)
 import Element exposing (..)
 import Collage exposing (..)
-import List
+import Text exposing (..)
 
 main =
-  toHtml <| collage 300 300
-    [makeSquare blue 75 ]
+  toHtml <| makeCollage 200 500
 
-makeSquare color size =
-  filled color (square size)
+message msg =
+  text (Text.color white (fromString msg))
+
+makeSquare color msg =
+  group [filled color (square 75), message msg ]
+
+makeCollage x y =
+  let
+    a = makeSquare blue "Bob"
+    b = move (0, -100) (makeSquare red "elm")
+  in
+    collage x y [a, b]
+
