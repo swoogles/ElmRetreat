a247f813daf801b1c483913f6b1777918c7c2137
Bruce Eckel <mindviewinc@gmail.com>
2016-10-08 13:16:02 -0600

    ran elm-format

--- Squares/Square.elm
+++ Squares/Square.elm
@@ -5,19 +5,25 @@ import Element exposing (..)
 import Collage exposing (..)
 import Text exposing (..)
 
+
 main =
-  toHtml <| makeCollage 200 500
+    toHtml <| makeCollage 200 500
+
 
 message msg =
-  text (Text.color white (fromString msg))
+    text (Text.color white (fromString msg))
+
 
 makeSquare color msg =
-  group [filled color (square 75), message msg ]
+    group [ filled color (square 75), message msg ]
+
 
 makeCollage x y =
-  let
-    a = makeSquare blue "Bob"
-    b = move (0, -100) (makeSquare red "elm")
-  in
-    collage x y [a, b]
+    let
+        a =
+            makeSquare blue "Bob"
 
+        b =
+            move ( 0, -100 ) (makeSquare red "elm")
+    in
+        collage x y [ a, b ]
